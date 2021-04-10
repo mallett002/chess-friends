147859293255bbc37cb577307b7c26382ac6a58d
Will Mallett <mallett002@gmail.com>
2021-04-10 13:48:20 -0500

    remove console logs

--- pages/home/home-screen.js
+++ pages/home/home-screen.js
@@ -1,8 +1,7 @@
 import * as React from 'react';
 import { Text, View } from 'react-native';
 
-export default function HomeScreen({navigation}) {
-  console.log({navigation});
+export default function HomeScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Home!</Text>
